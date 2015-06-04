d7165b08ea7a7b5bb8ac7efb14bb49be3c0251df
Sachin Sashital <ssashita@yahoo.com>
2015-06-04 21:15:42 +0530

    first

---
+++ catclicker.js
@@ -0,0 +1,43 @@
+var catClick = function(ct) {
+    var count =0;
+    var cat=ct;
+    return {
+        increment: function() {
+            ++count;
+            var label = 
+                document.getElementById("countOfCat"+cat.getAttribute("id"));
+            label.htmlFor = count;
+        },
+        getClickCount: function(){
+            return count;
+        },
+        name: function() {
+            return cat.getAttribute("name");
+        }
+    }
+};
+var imagePath = "https://lh3.ggpht.com/nlI91wYNCrjjNy5f-S3CmVehIBM4cprx-JFWOztLk7vFlhYuFR6YnxcT446AvxYg4Ab7M1Fy0twaOCWYcUk=s0#w=640&h=426";
+
+var ids = ["cat1", "cat2"];
+
+var renderCatImages= function(){
+    var i =0;
+    var len = ids.length;
+    for (;i<len;i++) {
+        alert("hi");
+        var cat = document.createElement("IMG");
+        cat.src=imagePath;
+        var div = document.createElement("DIV");
+        div.setAttribute("id",ids[i]);
+        var cClick = catClick(div);
+        var name =cClick.name();
+        var countElement = document.createElement("LABEL");
+        countElement.setAttribute("id","countOfCat"+ids[i]);
+        countElement.htmlFor = cClick.getClickCount();
+        div.innerHTML = name;
+        div.appendChild(countElement);
+        div.appendChild(cat);
+        cat.addEventListener("click", cClick.increment);
+        //document.appendChild(div);
+    }
+};
